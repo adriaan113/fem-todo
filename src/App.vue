bf5b1f2360d825eddf20148374c03dd7dcbad1df
Adriaan van der Ploeg <adriaan@MacBook-Air-van-Adriaan.local>
2021-08-27 22:51:49 +0200

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header :style="{ backgroundImage: 'url(' + bgLight + ')' }">
+        <div class="title">
+          <h1>to do</h1>
+          <img :src="nightBtn" alt="" v-if="night" @click="nightMode">
+          <img :src="dayBtn" alt="" v-else @click="nightMode">
+        </div>
+        <input type="text">
+    </header>
+    <main>
+      <toDoList msg="Welcome to Your Vue.js App"/>
+    </main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import toDoList from './components/toDoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    toDoList
+  },
+  data: function () {
+    return {
+      bgLight: require('./assets/bg.png'),
+      dayBtn: require('./assets/day.svg'),
+      nightBtn: require('./assets/night.svg'),
+      night: true,
+    }
+  },
+  methods:{
+    nightMode(){
+      this.night = !this.night;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import './global.scss';
+
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    background-color: $bg-light;
+    width: 100%;
+    height: 100vh;
+    font-family: 'Josefin Sans', sans-serif;
+    header{
+      height: 15vh;
+      background-position: center; 
+      background-repeat: no-repeat;
+      background-size: cover; 
+      justify-content: center;
+      align-items: center;
+      display: flex;
+      flex-flow: column nowrap;
+      .title{
+        display: flex;
+        width: 80%;
+        justify-content: space-between;
+        margin-bottom: 1.5rem;
+        max-width: 540px;
+        h1{
+          text-transform: uppercase;
+          color: white;
+        }
+        img{
+          cursor: pointer;
+        }
+      }
+      input{
+        width: 80%;
+        height: 2rem;
+        max-width: 540px;
+        border-radius: $br;
+        border-style: none;
+      }
+    }
+    
+  }
 </style>
---
+++ src/components/toDoList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="list">
+    <ul>
+      <li>
+       <input type="radio" name="" id="">
+       <p>iets</p>
+       <span>x</span>
+      </li>
+      <li>
+        <input type="radio" name="" id="">
+        <p>niets</p>
+        <span>x</span>
+      </li>
+      <li>
+        <input type="radio" name="" id="">
+        <p>dit</p>
+        <span>x</span>
+      </li>
+      <li>
+        <input type="radio" name="" id="">
+        <p>dat</p>
+        <span>x</span>
+      </li>
+      <li>
+        <input type="radio" name="" id="">
+        <p>koeka</p>
+        <span>x</span>
+      </li>
+    </ul>
+    <div>
+      <p>5 items left</p>
+      <p>all</p>
+      <p>active</p> 
+      <p>completed</p>
+      <p>clear completed</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toDoList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss"  scoped>
+
+
+</style>
